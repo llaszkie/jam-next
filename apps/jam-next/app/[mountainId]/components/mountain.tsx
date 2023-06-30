c44699b675af1dea1cbde4a9de2d47e5096703da
lukasz.laszkiewicz <lukasz.laszkiewicz@capgemini.com>
2023-06-30 13:55:24 +0200

    fix: make it look better

--- apps/jam-next/app/[mountainId]/components/mountain.tsx
+++ apps/jam-next/app/[mountainId]/components/mountain.tsx
@@ -28,10 +28,10 @@ export default async function Mountain({ mountainId }: { mountainId: string }) {
 
   return (
     <article>
-      <h3 className="p-5 text-lg text-center uppercase text-blue-900">
+      <h3 className="p-5 text-lg text-center uppercase text-cyan-900">
         {mountain.title}
       </h3>
-      <div className="relative w-5/6 h-96 mx-auto">
+      <div className="relative w-5/6 h-96 mx-auto max-w-xl">
         <Image
           className="sm:rounded-lg shadow-xl mx-auto shadow-gray-800"
           src={mountain.image}
@@ -42,29 +42,31 @@ export default async function Mountain({ mountainId }: { mountainId: string }) {
         ></Image>
       </div>
       <p className="p-5 m-5">{mountain.description}</p>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg m-5">
-        <table className="w-full text-sm text-left ">
-          <tbody>
-            <tr className="border-b border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium whitespace-nowrap  text-white bg-slate-600"
-              >
-                Height
-              </th>
-              <td className="px-6 py-4">{mountain.height}</td>
-            </tr>
-            <tr className="border-b -border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium whitespace-nowrap  text-white bg-slate-600"
-              >
-                Continent
-              </th>
-              <td className="px-6 py-4">{mountain.continent}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="px-5">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg m-5">
+          <table className="w-full text-sm text-left ">
+            <tbody>
+              <tr className="border-b border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium whitespace-nowrap  text-white bg-slate-600"
+                >
+                  Height
+                </th>
+                <td className="px-6 py-4">{mountain.height}</td>
+              </tr>
+              <tr className="border-b -border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium whitespace-nowrap  text-white bg-slate-600"
+                >
+                  Continent
+                </th>
+                <td className="px-6 py-4">{mountain.continent}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </article>
   );
